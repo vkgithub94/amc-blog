ba51f6f98327908031c189eba6382f60be41d8ef
Vikash <vikashp@orowealth.com>
2019-12-27 17:29:56 +0530

    Updates

---
+++ precache-manifest.5fbca34db7a59121d7615291df4cd1cf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce8dcb58156450c59772ce02c1593b2",
+    "url": "/amc-blog/index.html"
+  },
+  {
+    "revision": "a56976282d2d4af36048",
+    "url": "/amc-blog/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0151ce691af8a62dc45e",
+    "url": "/amc-blog/static/css/main.b234955c.chunk.css"
+  },
+  {
+    "revision": "a56976282d2d4af36048",
+    "url": "/amc-blog/static/js/2.c76bf57d.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/amc-blog/static/js/2.c76bf57d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0151ce691af8a62dc45e",
+    "url": "/amc-blog/static/js/main.80116944.chunk.js"
+  },
+  {
+    "revision": "2759a8f3eef859df55a7",
+    "url": "/amc-blog/static/js/runtime-main.6eacff11.js"
+  },
+  {
+    "revision": "09a607a078c9aaf45fdc1ab6a82321a4",
+    "url": "/amc-blog/static/media/Disclosure_Document_26Dec19.09a607a0.pdf"
+  },
+  {
+    "revision": "7d4ebc21bb183703dbf58e1e5b1148b5",
+    "url": "/amc-blog/static/media/Mobile-162x300.7d4ebc21.png"
+  },
+  {
+    "revision": "c66b5c1c8dcae38e9620ada4669d740c",
+    "url": "/amc-blog/static/media/apple-brands.c66b5c1c.svg"
+  },
+  {
+    "revision": "4a18ff406313d6c343938dc309cb22b4",
+    "url": "/amc-blog/static/media/direct.4a18ff40.png"
+  },
+  {
+    "revision": "d73ed150edbc0e67abd5a4aa2f714c4f",
+    "url": "/amc-blog/static/media/expert.d73ed150.png"
+  },
+  {
+    "revision": "e274581238709fcd099e88af0eb57087",
+    "url": "/amc-blog/static/media/google-play-brands.e2745812.svg"
+  },
+  {
+    "revision": "ead48a27903d07379d7cc9d6beb836f7",
+    "url": "/amc-blog/static/media/graph.ead48a27.png"
+  },
+  {
+    "revision": "d9049f461eb3dd661b7f77c6e56e7593",
+    "url": "/amc-blog/static/media/linkedin-brands.d9049f46.svg"
+  },
+  {
+    "revision": "f820760ce01f34920fa93a6744eb2733",
+    "url": "/amc-blog/static/media/nitin.f820760c.png"
+  },
+  {
+    "revision": "77cdb11773643e53f0aed8af0be7e28d",
+    "url": "/amc-blog/static/media/vaibhav.77cdb117.png"
+  },
+  {
+    "revision": "d181952484e52ccac8ba8d801ed00983",
+    "url": "/amc-blog/static/media/vijay.d1819524.png"
+  },
+  {
+    "revision": "1283699bd4e4488148896550f1d8bf55",
+    "url": "/amc-blog/static/media/yogesh.1283699b.png"
+  }
+]);
